04f8f039c1010b3882376b03be81d9ab73c4cc98
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2020-09-22 17:33:14 -0300

    commit

---
+++ capitulo1/fundamentos/erro.js
@@ -0,0 +1,17 @@
+function tratarErroELancar(erro){
+    throw new Error('...')
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.nome.toUpperCase() + '!!!');
+    } catch (e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('final');
+    }
+    
+}
+
+const obj = { nome: 'bruno' }
+imprimirNomeGritado(obj)
